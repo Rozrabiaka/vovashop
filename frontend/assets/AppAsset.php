62913a8abbde52b81ac48760ecf342173c3733c0
Rozrabiaka <denmaksiura@gmail.com>
2021-05-13 17:24:58 +0300

    remove fonts

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -13,7 +13,6 @@ class AppAsset extends AssetBundle
     public $baseUrl = '@web';
     public $css = [
         'css/site.css',
-        'css/fonts.css',
     ];
     public $js = [
         'js/main.js',
