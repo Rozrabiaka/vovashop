c0e69d1c754a29cc6afeb6b9c34146aba06d5abf
Rozrabiaka <denmaksiura@gmail.com>
2021-05-13 16:40:45 +0300

    йуцу

--- common/widgets/views/categoryMenu.php
+++ common/widgets/views/categoryMenu.php
@@ -1,7 +1,6 @@
 <?php
 /* @var $model backend\models\Categories */
 ?>
-
 <div class="categories-menu">
     <div class="header-bottom">
         <div class="header-sticky">
@@ -9,7 +8,7 @@
                 <div class="row col-md-12 col-sm-offset-1 hid">
                     <div class="row">
                         <!-- Header Menu Start -->
-                        <div class="col-sm-6 col-sm-offset-3 text-center">
+                        <div class="col-sm-6 col-sm-offset-2 text-center">
                             <div class="main-menu">
                                 <ul>
                                     <?php foreach ($model as $data): ?>
